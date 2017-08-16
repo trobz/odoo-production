c2ab9a3c23c10336497c4a689c20163d8f6a7485
Nguyen Van Thong <thongnv@trobz.com>
2017-08-16 15:20:28 +0700

    [ADD] - S#22986 - New menu Vendor Refunds;Avoirs fournisseur

---
+++ intercoop_addons/coop_purchase_invoicing/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.cooplalouve.fr/>)
+# @author: La Louve
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+{
+    'name': 'Coop - Purchare Invoicing',
+    'version': '9.0.1.0.0',
+    'category': 'Custom',
+    'summary': 'Coop - Purchare Invoicing.',
+    'author': 'La Louve',
+    'website': 'http://www.lalouve.net',
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'views/action.xml',
+        'views/menu.xml'
+    ],
+    'installable': True,
+}
