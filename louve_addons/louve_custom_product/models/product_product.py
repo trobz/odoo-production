893eee4ed2d4b3be534bfa36c0ef96259bc1f7fe
Tran Thanh Phuc <phuc@trobz.com>
2017-08-31 18:02:26 +0700

    [ADD] - S#14010 - Eurofeuille logo on Scale Label

--- louve_addons/louve_custom_product/models/product_product.py
+++ louve_addons/louve_custom_product/models/product_product.py
@@ -3,11 +3,46 @@
 # @author: La Louve
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    scale_logo_code = fields.Char(related="product_tmpl_id.scale_logo_code",
-                                  store="True")
+    scale_logo_code = fields.Char(string="Scale Logo Code", readonly=True)
+
+    @api.model
+    def create(self, vals):
+        vals['scale_logo_code'] = '1'
+        if 'label_ids' in vals:
+            label_ids_val = vals.get("label_ids", [])
+            if label_ids_val and isinstance(label_ids_val, list) and \
+                    len(label_ids_val[0]) == 3:
+                label_ids = label_ids_val[0][2]
+                labels = self.env['product.label'].browse(label_ids)
+                vals['scale_logo_code'] = labels and \
+                    labels[0].scale_logo_code or '1'
+
+        res = super(ProductProduct, self).create(vals)
+        return res
+
+    @api.multi
+    def write(self, vals):
+        if 'label_ids' in vals:
+            for product in self:
+                current_logo_code = product.scale_logo_code
+                res = super(ProductProduct, product).write(vals)
+
+                # Browse again to see check the value
+                updated_prod = self.browse(product.id)
+                new_logo_code = updated_prod.label_ids and \
+                    updated_prod.label_ids[0].scale_logo_code or '1'
+
+                # Only trigger the change if it is actually change
+                if current_logo_code != new_logo_code:
+                    res = super(ProductProduct, updated_prod).write(
+                        {'scale_logo_code': new_logo_code}
+                    )
+        else:
+            return super(ProductProduct, self).write(vals)
+        return res
