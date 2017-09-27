f541bfb722906e4f867e7b797feadb89fbe1f14f
Do Vuong Tien <tiendv@trobz.com>
2017-09-27 10:37:24 +0700

    [REF] - S#13474 - refactor source code as comments

--- intercoop_addons/partner_validate_email/models/res_config.py
+++ intercoop_addons/partner_validate_email/models/res_config.py
@@ -3,8 +3,8 @@
 # @author: La Louve (<http://www.lalouve.net/>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
 from openerp import fields, models, api, tools, _
+from openerp.tools.safe_eval import safe_eval
 
 
 class BaseConfigSettings(models.TransientModel):
@@ -16,13 +16,12 @@ class BaseConfigSettings(models.TransientModel):
 
     @api.multi
     def set_validate_email(self):
-        self.ensure_one()
         value = getattr(self, 'validate_email', False)
         self.env['ir.config_parameter'].set_param('validate_email', value)
 
     @api.multi
     def get_default_validate_email(self):
         config_param_env = self.env['ir.config_parameter']
-        avail_check = config_param_env.get_param('validate_email', False)
-        avail_check = avail_check and eval(avail_check) or False
+        avail_check = config_param_env.get_param('validate_email', 'False')
+        avail_check = safe_eval(avail_check)
         return {'validate_email': avail_check}
--- intercoop_addons/partner_validate_email/models/res_partner.py
+++ intercoop_addons/partner_validate_email/models/res_partner.py
@@ -7,7 +7,7 @@ import re
 from openerp import api, fields, models, tools, _
 from openerp.exceptions import Warning
 import logging
-
+from openerp.tools.safe_eval import safe_eval
 try:
     from validate_email import validate_email
     from DNS import TimeoutError
@@ -21,23 +21,24 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     @api.model
-    def validate_and_update_email(self, email, disable_validate_email=False):
+    def validate_email_address(self, email):
         '''
         @Function validate email.
             - Validate email format with regex.
             - Use library validate_email to check email exist or not.
         '''
+        disable = self._context.get('disable_validate_email', False)
         config_param_env = self.env['ir.config_parameter']
-        if disable_validate_email:
-            return email
         email_temp = email and re.sub('\s', '', email)
         valid = True
         message = ""
         if tools.single_email_re.match(email_temp):
+            if disable:
+                return email
             message = _("Email format correct.\n")
             avail_check = config_param_env.get_param('validate_email',
-                                                     False)
-            avail_check = avail_check and eval(avail_check) or False
+                                                     'False')
+            avail_check = safe_eval(avail_check)
             if not validate_email or not avail_check:
                 return email_temp
             else:
@@ -47,38 +48,34 @@ class ResPartner(models.Model):
                                            verify=True)
                 except TimeoutError as toe:
                     # time out when checking validating email domain.
-                    # but the format of email still be correct.
-                    # so pass this case.
+                    # It still be correct so pass this case.
                     valid = True
                     _logger.warn("[TimeoutError] - time out when checking " +
                                  "email '%s' - partner_id=" % (email, self.id))
-        else:
+        elif email_temp:
             valid = False
+        else:
+            # this case happen when user input all space in this field.
+            valid = True
         if valid:
             return email_temp
         else:
             if not message:
                 raise Warning(_("Email format is incorrect."))
             else:
-                message = _("Email format is correct but domain "
-                            "doesn't seem to have any mail server.")
-                raise Warning(message)
+                raise Warning(_("Email format is correct but domain " +
+                                "doesn't seem to have any mail server."))
 
     @api.model
     def create(self, vals):
-        disable = self._context.get('disable_validate_email', False)
         email = vals.get('email', False)
         if email:
-            vals.update({'email': self.validate_and_update_email(email,
-                                                                 disable)})
+            vals.update({'email': self.validate_email_address(email)})
         return super(ResPartner, self).create(vals)
 
     @api.multi
     def write(self, vals):
-        disable = self._context.get('disable_validate_email', False)
-        for rec in self:
-            email = vals.get('email', False)
-            if email:
-                vals.update({'email': rec.validate_and_update_email(email,
-                                                                    disable)})
+        email = vals.get('email', False)
+        if email:
+            vals.update({'email': self.validate_email_address(email)})
         return super(ResPartner, self).write(vals)
