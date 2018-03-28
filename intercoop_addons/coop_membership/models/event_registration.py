ab4a106c8f502d4f9b48fc341af990016436ed3d
tinvan94 <tinvan111294@gmail.com>
2018-03-28 12:00:17 +0700

    [ADD][F#26727][S#16092]Pre-registration web form [P1] + [ADD][F#26727][S#16092]Pre-registration web form [P1] + [ADD][F#27197][S#16596]Res.partner cleaning [P1] + [ADD][F#26627][S#16093]Member contract [P2]

---
+++ intercoop_addons/coop_membership/models/event_registration.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api, models, fields
+
+
+class event_registration(models.Model):
+    _inherit = 'event.registration'
+
+    is_discovery_meeting_event = fields.Boolean(
+        'Discovery Meeting Event',
+        related='event_id.is_discovery_meeting')
