83eac5fc23947f11d38fffef9d8fdb744a95272d
Nguyen Van Tin <tinnv@trobz.com>
2018-09-26 17:09:31 +0700

    [ADD][F#25737][S#15526]Exact Match

--- louve_addons/louve_custom_product/__openerp__.py
+++ louve_addons/louve_custom_product/__openerp__.py
@@ -43,7 +43,6 @@ Functionnalities
         # security
         'security/ir.model.access.csv',
         'views/product_label_view.xml',
-        'views/product_product_view.xml',
         'data/product_label.xml',
         'data/ir_actions_report_xml.xml',
         'data/report_paperformat.xml',
