ca169a5d00f035181cf45c01577a1b6f57813e33
Nguyen Anh Tuan <tuanna@trobz.com>
2018-10-08 10:45:24 +0700

    [ADD] Fix email alias

--- intercoop_addons/coop_memberspace/models/mail_thread.py
+++ intercoop_addons/coop_memberspace/models/mail_thread.py
@@ -44,39 +44,37 @@ class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
     @api.model
-    def verify_memberspace_alias(self, message, header, user_sent, email_from):
+    def verify_memberspace_alias(self, message, header, partner):
+        if not message.has_key(header):
+            return []
         MemberSpaceAlias = self.env['memberspace.alias']
-        Alias = self.env['mail.alias']
         alias_error = MemberSpaceAlias
         alias_pass = MemberSpaceAlias
+        email_pass = []
         mail_tmpl = self.env.ref(
             'coop_memberspace.email_inform_cannot_send_to_memberspace_alias')
-        if not message.has_key(header):
-            return alias_error, alias_pass
         rcpt_tos = ','.join([decode_header(message, header)])
-        local_parts = [e.split('@')[0].lower()
-            for e in tools.email_split(rcpt_tos)]
-        aliases = Alias.search([('alias_name', 'in', local_parts)])
-        memberspace_aliases = MemberSpaceAlias.search([
-            ('alias_id', 'in', aliases.ids)])
-        for memberspace_alias in memberspace_aliases:
-            if not user_sent:
-                alias_error |= memberspace_aliases
-                break
+        for e in tools.email_split(rcpt_tos):
+            local_part = e.split('@')[0].lower()
+            memberspace_alias = MemberSpaceAlias.search([
+                ('alias_name', '=', local_part)], limit=1)
+            if not memberspace_alias:
+                email_pass.append(e)
+                continue
             coordinators = memberspace_alias.shift_id.user_ids
             members = coordinators | memberspace_alias.shift_id.registration_ids.filtered(
                 lambda r: r.is_current_participant).mapped('partner_id')
             if memberspace_alias.type == 'team' and \
-                user_sent not in coordinators:
+                partner not in coordinators:
                 alias_error |= memberspace_alias
-                if user_sent in members and mail_tmpl:
+                if partner in members and mail_tmpl:
                     email_add = memberspace_alias.alias_name + '@' + \
                         memberspace_alias.alias_domain
                     template_values = {
-                        'email_to': email_from,
+                        'email_to': partner.email,
                         'email_from': self.env.user.company_id.email,
                         'email_cc': False,
-                        'lang': user_sent.lang,
+                        'lang': partner.lang,
                         'auto_delete': True,
                         'partner_to': False,
                     }
@@ -85,15 +83,16 @@ class MailThread(models.AbstractModel):
                         self.env.user.id, force_send=True)
                 continue
             elif memberspace_alias.type == 'coordinator' and \
-                user_sent not in members:
+                partner not in members:
                 alias_error |= memberspace_alias
                 continue
             alias_pass |= memberspace_alias
-        if alias_error:
-            new_header = ','.join([ma.alias_name + '@' + ma.alias_domain
+        email_pass.extend([ma.alias_name + '@' + ma.alias_domain
                 for ma in alias_pass])
+        if alias_error:
+            new_header = ','.join(email_pass)
             message.replace_header(header, new_header)
-        return alias_error, alias_pass
+        return email_pass
 
     @api.model
     def message_route(self, message, message_dict, model=None,
@@ -104,38 +103,36 @@ class MailThread(models.AbstractModel):
         # Memberspace workflow
         if '<' in email_from:
             email_from = email_from.split('<')[1][:-1]
-        user_send_email = self.env['res.partner'].search([
+        partner = self.env['res.partner'].search([
             ('email', '=', email_from)
         ], limit=1)
-        error_memberspace_aliases = self.env['memberspace.alias']
-        pass_memberspace_aliases = self.env['memberspace.alias']
+        if not partner:
+            return super(MailThread, self).message_route(
+                message, message_dict, model=model, thread_id=thread_id,
+                custom_values=custom_values)
+        emails_pass = []
         # Check header with key = To
-        alias_error, alias_pass = self.verify_memberspace_alias(
-            message, 'To', user_send_email, email_from)
-        pass_memberspace_aliases |= alias_pass
-        error_memberspace_aliases |= alias_error
+        email_pass = self.verify_memberspace_alias(
+            message, 'To', partner)
+        emails_pass.extend(email_pass)
         # Check header with key = Delivered-To
-        alias_error, alias_pass = self.verify_memberspace_alias(
-            message, 'Delivered-To', user_send_email, email_from)
-        pass_memberspace_aliases |= alias_pass
-        error_memberspace_aliases |= alias_error
+        email_pass = self.verify_memberspace_alias(
+            message, 'Delivered-To', partner)
+        emails_pass.extend(email_pass)
         # Check header with key = Cc
-        alias_error, alias_pass = self.verify_memberspace_alias(
-            message, 'Cc', user_send_email, email_from)
-        pass_memberspace_aliases |= alias_pass
-        error_memberspace_aliases |= alias_error
+        email_pass = self.verify_memberspace_alias(
+            message, 'Cc', partner)
+        emails_pass.extend(email_pass)
         # Check header with key = Resent-To
-        alias_error, alias_pass = self.verify_memberspace_alias(
-            message, 'Resent-To', user_send_email, email_from)
-        pass_memberspace_aliases |= alias_pass
-        error_memberspace_aliases |= alias_error
+        email_pass = self.verify_memberspace_alias(
+            message, 'Resent-To', partner)
+        emails_pass.extend(email_pass)
         # Check header with key = Resent-Cc
-        alias_error, alias_pass = self.verify_memberspace_alias(
-            message, 'Resent-Cc', user_send_email, email_from)
-        pass_memberspace_aliases |= alias_pass
-        error_memberspace_aliases |= alias_error
+        email_pass = self.verify_memberspace_alias(
+            message, 'Resent-Cc', partner)
+        emails_pass.extend(email_pass)
         
-        if not pass_memberspace_aliases:
+        if not emails_pass:
             return []
         return super(MailThread, self).message_route(
             message, message_dict, model=model, thread_id=thread_id,
