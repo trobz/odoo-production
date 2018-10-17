b7246bd6f21d87df3ccaba4731901d0767e21b8b
Phat Nguyen Tan <phatnt@trobz.com>
2018-10-17 17:50:51 +0700

    [FIX]Update method to compute product type in stock.quant model by using cron jobs

--- louve_addons/louve_custom_inventory/models/stock_quant.py
+++ louve_addons/louve_custom_inventory/models/stock_quant.py
@@ -1,12 +1,43 @@
 # -*- coding: utf-8 -*-
 
-from openerp import fields, models
+from openerp import api, fields, models
+from openerp.addons.connector.queue.job import job
+from openerp.addons.connector.session import ConnectorSession
 
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    product_type = fields.Selection(
-        related='product_id.type',
+    product_type = fields.Char(
+        compute='_compute_product_type',
         store=True
     )
+
+    def _compute_product_type(self):
+        for quant in self:
+            quant.product_type = quant.product_id.type
+        return True
+
+    # Custom Section
+    @api.multi
+    def update_product_type(self):
+        session_ids = self.ids
+        num_session_per_job = 500
+        splited_session_list = \
+            [session_ids[i: i + num_session_per_job]
+             for i in range(0, len(session_ids), num_session_per_job)]
+        # Prepare session for job
+        session = ConnectorSession(self._cr, self._uid)
+        # Create jobs
+        for session_list in splited_session_list:
+            update_product_type_stock_quant_session_job.delay(
+                session, 'stock.quant', session_list)
+        return True
+
+
+@job
+def update_product_type_stock_quant_session_job(
+        session, model_name, session_list):
+    """ Job for compute cycle """
+    quants = session.env[model_name].browse(session_list)
+    quants._compute_product_type()
