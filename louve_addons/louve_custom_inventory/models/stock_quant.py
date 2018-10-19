b37c15c2c1c68a08e594b19d66cb2f7160b4ab8b
Phat Nguyen Tan <phatnt@trobz.com>
2018-10-19 12:25:58 +0700

    [REF] Refactor method 'update_product_type' of stock.quant

--- louve_addons/louve_custom_inventory/models/stock_quant.py
+++ louve_addons/louve_custom_inventory/models/stock_quant.py
@@ -21,17 +21,17 @@ class StockQuant(models.Model):
     # Custom Section
     @api.multi
     def update_product_type(self):
-        session_ids = self.ids
-        num_session_per_job = 500
-        splited_session_list = \
-            [session_ids[i: i + num_session_per_job]
-             for i in range(0, len(session_ids), num_session_per_job)]
+        quants = self.ids
+        num_quant_per_job = 500
+        splited_quant_list = \
+            [quants[i: i + num_quant_per_job]
+             for i in range(0, len(quants), num_quant_per_job)]
         # Prepare session for job
         session = ConnectorSession(self._cr, self._uid)
         # Create jobs
-        for session_list in splited_session_list:
+        for quant_list in splited_quant_list:
             update_product_type_stock_quant_session_job.delay(
-                session, 'stock.quant', session_list)
+                session, 'stock.quant', quant_list)
         return True
 
 
