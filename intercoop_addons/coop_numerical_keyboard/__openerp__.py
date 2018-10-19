94fd6525c15a486badb25902da5ef24ca7e0c096
Phat Nguyen Tan <phatnt@trobz.com>
2018-10-19 15:40:33 +0700

    [REF] Refactor structure of the new module coop_numerical_keyboard

--- intercoop_addons/coop_numerical_keyboard/__openerp__.py
+++ intercoop_addons/coop_numerical_keyboard/__openerp__.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2018-Today: Trobz (<http://www.trobz.com/>)
+# Copyright (C) 2018-Today: La Louve (<https://cooplalouve.fr>)
+# @author: Simon Mas (linkedin.com/in/simon-mas)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
 {
     'name': 'Coop Numerical Keyboard',
-    'version': '0.0.0.1',
-    'category': 'Custom',
-    'author': 'Trobz',
-    'website': 'http://www.trobz.com',
+    'version': '9.0.1.0.0',
+    'category': 'Tools',
+    'summary': 'Create a new POS numerical keyboard',
+    'author': 'Simon Mas, Phat Nguyen Tan, Odoo Community Association (OCA)',
+    'website': 'https://www.druidoo.io',
     'depends': [
         'point_of_sale'
     ],
     'data': [
+        # Views
         'views/templates.xml',
     ],
     'qweb': [
         'static/src/xml/widget.xml'
     ],
     'installable': True,
+    'license': 'AGPL-3',
 }
