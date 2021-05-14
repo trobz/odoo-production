4d8c2dd49298b641cff02bf5def9b643a0c6612f
Nguyen Minh Chien <chien@trobz.com>
2021-05-14 03:50:54 +0000

    [UPD] update bank journal for the check deposit

---
+++ coop_account_check_deposit/migrations/12.0.1.1.1/post-migration.py
@@ -0,0 +1,16 @@
+def migrate(cr, version):
+    if not version:
+        return
+    cr.execute("""
+        WITH raw_rows AS (
+            SELECT a.id,
+                b.journal_id
+            FROM account_check_deposit a
+                JOIN account_move b ON a.move_id = b.id
+            WHERE a.bank_journal_id ISNULL
+        )
+        UPDATE account_check_deposit d
+        SET bank_journal_id = raw_rows.journal_id
+        FROM raw_rows
+        WHERE d.id = raw_rows.id
+    """)
