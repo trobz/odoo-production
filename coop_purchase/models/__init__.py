9fe18091853d1cfd67ae8eb16fe87b936d4d43ba
Nguyen Minh Chien <chien@trobz.com>
2021-09-22 09:01:35 +0000

    [REF]  Separate Internal Reference and Product Name when fused

--- coop_purchase/models/__init__.py
+++ coop_purchase/models/__init__.py
@@ -7,5 +7,4 @@ from . import product_supplierinfo
 from . import res_partner
 from . import stock_picking
 from . import purchase_bill
-from . import product_product
 from . import stock_inventory_line
