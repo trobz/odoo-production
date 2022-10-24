a2954df54a5bc1c57cf4fbdc65fa9146b01d8e77
Nguyen Minh Chien <chien@trobz.com>
2022-10-24 02:34:31 +0000

    [ADD] S#26794 - [Lalouve] Rights and Roles

---
+++ foodcoop_data_role/models/pos_session.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Coop IT Easy SCRLfs
+# @author Pierrick Brun <pierrick.brun@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class PosSession(models.Model):
+    _inherit = "pos.session"
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form',
+            toolbar=False, submenu=False):
+        res = super(PosSession, self).fields_view_get(
+            view_id, view_type, toolbar, submenu)
+        if self.user_has_groups(
+                "foodcoop_data_role.group_member_accountant_restrict") and \
+            res.get("toolbar", {}).get("action"):
+            del res["toolbar"]["action"]
+        return res
