11e5ed55bacab9228de7b26e8cbbde36c521aedc
Nguyen Minh Chien <chien@trobz.com>
2023-07-10 04:44:05 +0000

    [UPD] S#T35374 - Modify some of the text/labels of the interface to remove that notion of payment e-commerce

---
+++ website_sale_payment_term/model/sale_order.py
@@ -0,0 +1,12 @@
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        super().onchange_partner_id()
+        if not self.payment_term_id and self.website_id.payment_term_id:
+            self.payment_term_id = self.website_id.payment_term_id
