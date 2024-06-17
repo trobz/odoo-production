c2090b433901bd56858d48fd487440bd7d2af457
Nguyen Minh Chien <chien@trobz.com>
2024-06-17 02:47:49 +0000

    S#T36507 - [SQQ-VDM] Adjust the feature Credit Member

---
+++ pos_payment_credit_pin/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Point of Sale Payment Credit > Require Manager PIN',
+    'version': '12.0.1.0.0',
+    'category': 'Point Of Sale',
+    'author': 'Trobz',
+    'license': 'AGPL-3',
+    'depends': [
+        'pos_payment_credit',
+    ],
+    'data': [
+        "views/assets.xml",
+    ],
+    "qweb": [],
+    'installable': True,
+}
