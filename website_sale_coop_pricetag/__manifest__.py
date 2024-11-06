837db400652e54385559bc5a2d52f27470fd5046
Nguyen Minh Chien <chien@trobz.com>
2024-11-06 12:13:53 +0000

    F#T64434 - E-commerce website: show price / kg (or / L)

---
+++ website_sale_coop_pricetag/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Website Sale Show Pricetag',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'author': 'Trobz',
+    'website': 'https://trobz.com',
+    'license': 'AGPL-3',
+    'summary': 'Show Pricetag on the shop',
+    'depends': [
+        'website_sale',
+        'coop_default_pricetag'
+    ],
+    'data': [
+        'views/website_sale_template.xml'
+    ],
+    'installable': True,
+}
