8a98748377e8f87fad478f4447385fc81d5703af
Nguyen Minh Chien <chien@trobz.com>
2024-12-26 10:04:23 +0000

    F#T64734 - [otsokop] Enhance Inventory Barcode Wizard

---
+++ stock_inventory_barcode_qty_update/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Stock Inventory Barcode - Update Qty',
+    'version': '12.0.1.0.0',
+    'category': 'Custom',
+    'description': """""",
+    'author': 'Trobz',
+    'website': 'https://trobz.com',
+    'depends': [
+        'stock_inventory_barcode',
+    ],
+    'data': [
+        "view/view_stock_inventory.xml",
+        "wizard/stock_inventory_barcode.xml",
+    ],
+    'test': [],
+    'installable': True,
+}
---
+++ stock_inventory_barcode_qty_update/models/stock_inventory.py
@@ -0,0 +1,11 @@
+
+from odoo import fields, models
+
+
+class StockInventory(models.Model):
+    _inherit = "stock.inventory"
+
+    scan_barcode_mode = fields.Selection([
+        ("add", "Add Qty"),
+        ("change", "Change Qty")
+    ], default="add")
