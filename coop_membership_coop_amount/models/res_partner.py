a074eb841755a1f4cdbe1cc046f3dc121d3f9209
Nguyen Minh Chien <chien@trobz.com>
2025-02-28 04:03:44 +0000

    F#T65108 - [Chaudron] Add coop_amount and amount_subscription in the res_partner for generating BI reports from metabase

--- coop_membership_coop_amount/models/res_partner.py
+++ coop_membership_coop_amount/models/res_partner.py
@@ -5,17 +5,48 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    coop_amount = fields.Float(compute="_compute_coop_amount")
+    fundraising_invoice_ids = fields.One2many(
+        "account.invoice", "partner_id",
+        domain=[
+            ("type", "in", ("out_invoice", "out_refund")),
+            ("is_capital_fundraising", "=", True),
+            ("state", "in", ("paid", "open"))
+        ]
+    )
+    fundraising_journal_item_ids = fields.One2many(
+        "account.move.line", "partner_id",
+        domain=lambda self: self._get_fundraising_item_domain()
+    )
 
-    def _compute_coop_amount(self):
-        JournalItem = self.env["account.move.line"]
+    amount_subscription = fields.Float(store=True)
+    coop_amount = fields.Float(
+        compute="_compute_coop_amount", 
+        store=True
+    )
+
+    def _get_fundraising_item_domain(self):
         categories = self.env["capital.fundraising.category"].sudo().search([])
         capital_accounts = categories.mapped("capital_account_id")
-        domain = [
+        return [
             ("move_id.state", "=", "posted"),
             ("account_id", "in", capital_accounts.ids),
-            ("partner_id", "in", self.ids)
         ]
+
+    @api.depends(
+        "fundraising_invoice_ids", "fundraising_invoice_ids.state",
+        "fundraising_invoice_ids.invoice_line_ids"
+    )
+    def _compute_amount_subscription(self):
+        super()._compute_amount_subscription()
+
+    @api.depends(
+        "fundraising_journal_item_ids", "fundraising_journal_item_ids.move_id.state",
+        "fundraising_journal_item_ids.account_id","fundraising_journal_item_ids.partner_id",
+    )
+    def _compute_coop_amount(self):
+        JournalItem = self.env["account.move.line"]
+        domain = self._get_fundraising_item_domain()
+        domain.append(("partner_id", "in", self.ids))
         datas = JournalItem.read_group(
             domain, ["other_balance"], ["partner_id"]
         )
