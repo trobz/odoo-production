948104ed338edd872bbf6276fd3cf9ad91f9fda5
Nguyen Minh Chien <chien@trobz.com>
2025-03-13 09:34:01 +0000

    F#T65178 - [Chaudron] POS /lock access to discount

---
+++ pos_pricelist_selection_right/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Point of Sale - Restrict Pricelists to Partner",
+    "summary": "Restrict the pricelists to the partner"
+    " displayed in the Point of Sale front-end UI",
+    "version": "12.0.1.0.0",
+    "category": "Point of Sale",
+    "author": "Trobz",
+    "website": "https://trobz.com",
+    "license": "AGPL-3",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "qweb": [],
+    "installable": True,
+}
