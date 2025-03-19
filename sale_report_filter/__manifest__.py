a8a49440db150e1b7faccf6cdddd6925fe43ca1b
Nguyen Minh Chien <chien@trobz.com>
2025-03-19 04:44:48 +0000

    F#T65197 - [Lalouve] Sales Analysis Default Options and Filters

---
+++ sale_report_filter/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Statistics Filter',
+    'version': '12.0.1.0.0',
+    'category': 'Custom',
+    'author': 'Trobz',
+    'website': 'http://www.trobz.com',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'view/sales.xml',
+    ],
+    'qweb': [],
+    'test': [],
+    'installable': True,
+}
