158a47a50f801dd979741939c68d4ec901fa5adc
Nguyen Minh Chien <chien@trobz.com>
2025-05-30 05:14:52 +0000

    F#T65556 - [Chaudron] BdM: deactivate an automatic email which is not appropriate

---
+++ mail_template_active/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Mail template active',
+    'version': '12.0.1.0.0',
+    'category': 'Custom',
+    'description': """Add new field 'active' to mail.template model to allow
+    deactivating mail templates without deleting them.""",
+    'summary': 'Add active field to mail templates',
+    'license': 'AGPL-3',
+    'author': 'Trobz',
+    'website': 'https://trobz.com',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        "views/mail_template_views.xml",
+    ],
+    'test': [],
+}
