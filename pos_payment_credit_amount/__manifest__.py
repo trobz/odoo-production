cd224bc02a4324e4ec7fdc8dddbe500fe39722f3
Nguyen Minh Chien <chien@trobz.com>
2026-02-25 18:10:58 +0700

    [UPD] pos_payment_credit_amount: authors

--- pos_payment_credit_amount/__manifest__.py
+++ pos_payment_credit_amount/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Point of Sale Payment Credit: Amount Handling",
     "version": "18.0.1.0.0",
     "category": "Point Of Sale",
-    "author": "Trobz, Odoo Community Association (OCA)",
+    "author": "Trobz",
     "website": "https://github.com/AwesomeFoodCoops/odoo-production",
     "license": "AGPL-3",
     "depends": [
