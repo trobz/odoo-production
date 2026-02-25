12c9f3f3d8d3e6f570b51ffcf5ce00efb802d1d7
Nguyen Minh Chien <chien@trobz.com>
2026-02-25 15:49:58 +0700

    [ADD] pre-commit settings

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+
+const config = {
+  plugins: [require.resolve("@prettier/plugin-xml")],
+  bracketSpacing: false,
+  printWidth: 88,
+  proseWrap: "always",
+  semi: true,
+  trailingComma: "es5",
+  xmlWhitespaceSensitivity: "preserve",
+};
+
+module.exports = config;
