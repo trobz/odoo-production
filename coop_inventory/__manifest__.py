a5fb5459c4d07a76a121196dbde79e3bd7de4b47
Kimkhoi3010 <kkimkhoi3010@gmail.com>
2026-02-27 11:12:10 +0700

    [MIG] coop_inventory: Migration to 18.0

--- coop_inventory/__manifest__.py
+++ coop_inventory/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Coop - Inventory",
-    "version": "12.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "stock",
     "author": "La Louve",
     "website": "https://github.com/AwesomeFoodCoops/odoo-production",
@@ -13,9 +13,11 @@
     "depends": [
         "purchase_stock",
         "purchase_package_qty",
+        "stock_inventory",
     ],
     "data": [
-        "view/report_stockinventory.xml",
+        "data/report_paperformat.xml",
+        "report/report_stockinventory.xml",
         "view/view_stock_inventory.xml",
         "view/view_stock_picking.xml",
     ],
--- coop_inventory/models/__init__.py
+++ coop_inventory/models/__init__.py
@@ -4,5 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
 from . import purchase_order
+from . import purchase_order_line
+from . import stock_inventory
 from . import stock_picking
-from . import stock_inventory_line
+from . import stock_quant
