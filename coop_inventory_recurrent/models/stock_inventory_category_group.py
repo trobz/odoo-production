7177a0b91594d7486034c6c4dc109d4115f191b7
khoi <kkimkhoi3010@gmail.com>
2026-03-03 14:49:08 +0700

    [IMP] coop_inventory_recurrent: pre-commit auto fixes

--- coop_inventory_recurrent/models/stock_inventory_category_group.py
+++ coop_inventory_recurrent/models/stock_inventory_category_group.py
@@ -8,24 +8,15 @@ class StockInventoryCategoryGroup(models.Model):
     _description = "Category Group"
 
     name = fields.Char(required=True)
-    category_ids = fields.Many2many(
-        'product.category', string='Product categories'
-    )
-    line_ids = fields.One2many(
-        "stock.inventory.category.group.line",
-        "group_id"
-    )
+    category_ids = fields.Many2many("product.category", string="Product categories")
+    line_ids = fields.One2many("stock.inventory.category.group.line", "group_id")
 
-    @api.onchange('category_ids')
+    @api.onchange("category_ids")
     def onchange_category_ids(self):
-        categs = self.mapped('line_ids.category_id')
-        GroupLine = self.env['stock.inventory.category.group.line']
+        categs = self.mapped("line_ids.category_id")
+        GroupLine = self.env["stock.inventory.category.group.line"]
         for categ in self.category_ids:
             if categ not in categs:
-                line = GroupLine.new({
-                    'category_id': categ.id,
-                    'copies': '2'
-                })
-                line.default_get(
-                    GroupLine.fields_get().keys())
+                line = GroupLine.new({"category_id": categ.id, "copies": "2"})
+                line.default_get(GroupLine.fields_get().keys())
                 self.line_ids |= line
