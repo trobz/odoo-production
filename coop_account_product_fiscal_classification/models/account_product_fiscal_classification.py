4040fd747bdf364af20aa48a0411d4cbc1db1f2c
phucph <phucph@trobz.com>
2026-03-09 11:59:44 +0700

    [MIG] coop_account_product_fiscal_classification: Migration to 18.0

--- coop_account_product_fiscal_classification/models/account_product_fiscal_classification.py
+++ coop_account_product_fiscal_classification/models/account_product_fiscal_classification.py
@@ -1,18 +1,11 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountProductFiscalClassification(models.Model):
-    _inherit = 'account.product.fiscal.classification'
+    _inherit = "account.product.fiscal.classification"
 
-    income_account_id = fields.Many2one(
-        'account.account',
-        string='Income Account',
-    )
-    expense_account_id = fields.Many2one(
-        'account.account',
-        string='Expense Account',
-    )
+    income_account_id = fields.Many2one("account.account")
+    expense_account_id = fields.Many2one("account.account")
     # Make tax required
     sale_tax_ids = fields.Many2many(required=True)
     purchase_tax_ids = fields.Many2many(required=True)
--- coop_account_product_fiscal_classification/readme/DESCRIPTION.rst
+++
@@ -1,2 +0,0 @@
-This module Inherit the OCA module account_product_fiscal_classification.
-
