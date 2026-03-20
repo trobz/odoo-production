f718e9e093569cbf3227a4b92c57a348399e453b
Nguyen Minh Chien <chien@trobz.com>
2026-03-20 17:13:29 +0700

    [UPD] coop_account_check_deposit: add is_reconcile field

--- coop_account_check_deposit/models/account_check_deposit.py
+++ coop_account_check_deposit/models/account_check_deposit.py
@@ -1,41 +1,22 @@
 from odoo import api, fields, models
 
 
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
+class AccountCheckDeposit(models.Model):
+    _inherit = "account.check.deposit"
 
-    check_holder_name = fields.Char()
-    check_deposit_id = fields.Many2one(index=True)
+    is_reconcile = fields.Boolean(
+        compute="_compute_is_reconcile", store=True, string="Reconciled"
+    )
 
-    @api.onchange("partner_id")
-    def onchange_partner_id(self):
-        change_check_holder_name = self._context.get("change_check_holder_name", False)
-        if self.partner_id and not self.check_holder_name:
-            if change_check_holder_name or self.check_deposit_id:
-                self.check_holder_name = self.partner_id.display_name
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        res = super().create(vals_list)
-        res.with_context(create_mode=True).update_check_holder_name()
-        return res
-
-    def write(self, vals):
-        res = super().write(vals)
-        if not self._context.get("create_mode"):
-            self.update_check_holder_name()
-        return res
-
-    def update_check_holder_name(self):
-        """
-        Update check holder name for item was generate from deposit
-        """
-        for record in self:
-            if (
-                record.check_deposit_id
-                and record.partner_id
-                and not record.check_holder_name
-            ):
-                record.check_holder_name = record.partner_id.display_name
-            elif not record.check_deposit_id and record.check_holder_name:
-                record.check_holder_name = ""
+    @api.depends("bank_journal_id", "move_id")
+    def _compute_is_reconcile(self):
+        for rec in self:
+            reconcile = False
+            for line in rec.move_id.line_ids:
+                if (
+                    line.account_id.id != rec.bank_journal_id.default_account_id.id
+                    and line.reconciled
+                ):
+                    reconcile = True
+                    break
+            rec.is_reconcile = reconcile
---
+++ coop_account_check_deposit/models/account_move_line.py
@@ -0,0 +1,41 @@
+from odoo import api, fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    check_holder_name = fields.Char()
+    check_deposit_id = fields.Many2one(index=True)
+
+    @api.onchange("partner_id")
+    def onchange_partner_id(self):
+        change_check_holder_name = self._context.get("change_check_holder_name", False)
+        if self.partner_id and not self.check_holder_name:
+            if change_check_holder_name or self.check_deposit_id:
+                self.check_holder_name = self.partner_id.display_name
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        res.with_context(create_mode=True).update_check_holder_name()
+        return res
+
+    def write(self, vals):
+        res = super().write(vals)
+        if not self._context.get("create_mode"):
+            self.update_check_holder_name()
+        return res
+
+    def update_check_holder_name(self):
+        """
+        Update check holder name for item was generate from deposit
+        """
+        for record in self:
+            if (
+                record.check_deposit_id
+                and record.partner_id
+                and not record.check_holder_name
+            ):
+                record.check_holder_name = record.partner_id.display_name
+            elif not record.check_deposit_id and record.check_holder_name:
+                record.check_holder_name = ""
