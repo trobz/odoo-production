32324f35b3a2ec232ea1c2c2e21cb470b108af57
Nguyen Minh Chien <chien@trobz.com>
2026-03-24 10:41:40 +0700

    [UPD] coop_print_badge

--- coop_print_badge/models/res_company.py
+++ coop_print_badge/models/res_company.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.tools.safe_eval import safe_eval
 
 
 class ResCompany(models.Model):
@@ -13,6 +14,15 @@ class ResCompany(models.Model):
 
     @api.model
     def get_trigger_badge_reprint_fields(self):
-        image_field = self.env.ref("base.field_res_partner__image_1920")
-        name_field = self.env.ref("base.field_res_partner__name")
-        return [image_field.id, name_field.id]
+        field_str = self.env['ir.config_parameter'].sudo().get_param(
+            'reprint_change_field_ids', '[]')
+        field_ids = safe_eval(field_str)
+        if not field_ids:
+            # Default fields to trigger badge reprinting
+            field_ids = self.env['ir.model.fields'].search(
+                [
+                    ('model_id.model', '=', 'res.partner'),
+                    ('name', 'in', ['image_1920', 'name']),
+                ]
+            ).ids
+        return field_ids
