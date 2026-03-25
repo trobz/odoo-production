e5d07c6c661077154386316aedc8925ac8e943a2
khoi <kkimkhoi3010@gmail.com>
2026-03-25 17:05:39 +0700

    [FIX] account_financial_report_custom: skip archived analytic accounts

--- account_financial_report_custom/wizard/__init__.py
+++ account_financial_report_custom/wizard/__init__.py
@@ -1,4 +1,3 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import report
-from . import wizard
+from . import general_ledger_wizard
---
+++ account_financial_report_custom/wizard/general_ledger_wizard.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class GeneralLedgerReportWizardCustom(models.TransientModel):
+
+    _inherit = "general.ledger.report.wizard"
+
+    def _get_atr_from_dict(self, obj_id, data, key):
+        values = data.get(obj_id) or data.get(str(obj_id))
+        if not values:
+            return ""
+        return values.get(key, "")
