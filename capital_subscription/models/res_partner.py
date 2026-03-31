90de3156c3129c686105ebda4634507cb26f4770
Nguyen Minh Chien <chien@trobz.com>
2026-03-31 15:04:45 +0700

    [FIX] capital_subscription: domain warning when creating

--- capital_subscription/models/res_partner.py
+++ capital_subscription/models/res_partner.py
@@ -3,7 +3,7 @@
 # @author: Julien Weste
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -19,6 +19,7 @@ class ResPartner(models.Model):
     )
 
     # Compute section
+    @api.depends("invoice_ids", "invoice_ids.is_capital_fundraising", "invoice_ids.state")
     def _compute_amount_subscription(self):
         inv_obj = self.env["account.move"].sudo()
         for partner in self:
