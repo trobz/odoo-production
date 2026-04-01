ab89de3417204af777318a4dfceab3d7e9bae2fd
Nguyen Minh Chien <chien@trobz.com>
2026-04-01 13:10:37 +0700

    [FIX]: coop_produce: apply qty

--- coop_produce/__manifest__.py
+++ coop_produce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Coop Produce",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Warehouse",
     "summary": "Coop Produce",
     "author": "La Louve, Druidoo",
