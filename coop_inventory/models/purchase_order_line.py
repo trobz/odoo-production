e1a444977ef02746c7f88fb57aa0ef883f0aeac3
khoi <kkimkhoi3010@gmail.com>
2026-04-02 10:54:51 +0700

    [FIX] coop_inventory: clear packaging fields for consumable products on PO line

--- coop_inventory/models/purchase_order_line.py
+++ coop_inventory/models/purchase_order_line.py
@@ -4,6 +4,13 @@ from odoo import models
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
+    def _product_id_change(self):
+        res = super()._product_id_change()
+        if self.product_id and not self.product_id.is_storable:
+            self.product_packaging_id = False
+            self.price_policy = "uom"
+        return res
+
     def _prepare_stock_moves(self, picking):
         self.ensure_one()
         if not self.product_id.is_storable:
