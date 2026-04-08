28073e5b65bb0052cff0ca24d6ff77b9b89fbba2
khoi <kkimkhoi3010@gmail.com>
2026-04-08 15:57:41 +0700

    [IMP] coop_asset: pre-commit auto fixes

--- coop_asset/models/account_asset_profile.py
+++ coop_asset/models/account_asset_profile.py
@@ -1,8 +1,7 @@
+from odoo import fields, models
 
-from odoo import models, fields, api, _
 
 class AccountAssetProfile(models.Model):
-    _inherit = 'account.asset.profile'
+    _inherit = "account.asset.profile"
 
-    journal_id = fields.Many2one(
-        domain="[('company_id', '=', company_id)]")
+    journal_id = fields.Many2one(domain="[('company_id', '=', company_id)]")
