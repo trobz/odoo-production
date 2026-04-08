00fab66b493f472fb0f238779544b14d6a81e3ae
khoi <kkimkhoi3010@gmail.com>
2026-04-08 15:57:41 +0700

    [IMP] coop_asset: pre-commit auto fixes

--- coop_asset/__manifest__.py
+++ coop_asset/__manifest__.py
@@ -1,17 +1,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Coop - Asset',
-    'version': '12.0.1.0.0',
-    'category': 'Custom',
-    'summary': 'Custom Asset for Foodcoop',
-    'author': 'Trobz',
-    'website': 'https://www.trobz.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'account_asset_management',
+    "name": "Coop - Asset",
+    "version": "12.0.1.0.0",
+    "category": "Custom",
+    "summary": "Custom Asset for Foodcoop",
+    "author": "Trobz",
+    "website": "https://github.com/AwesomeFoodCoops/odoo-production",
+    "license": "AGPL-3",
+    "depends": [
+        "account_asset_management",
     ],
-    'data': [],
-    'installable': True,
-    'application': False,
+    "data": [],
+    "installable": True,
+    "application": False,
 }
