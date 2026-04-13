664a84bca4ef6975841e920f33647646c2e4def2
phucph <phucph@trobz.com>
2026-04-13 12:21:54 +0700

    [FIX] coop_print_badge: ignore field trigger not found

--- coop_print_badge/migrations/18.0.1.0.0/post-migration.py
+++ coop_print_badge/migrations/18.0.1.0.0/post-migration.py
@@ -17,12 +17,21 @@ def migrate(cr, version):
 
     _logger.info("Starting post-migration data adjustments...")
 
+    Company = env["res.company"]
     # Set reprint_change_field_ids to default value for existing companies
-    env["res.company"].search([]).write(
+    trigger_field_ids = Company.get_trigger_badge_reprint_fields()
+    trigger_fields = env["ir.model.fields"].search([("id", "in", trigger_field_ids)])
+
+    # Add image_1920 to the list of fields that trigger badge reprint
+    image_field = env["ir.model.fields"].search(
+        [("model", "=", "res.partner"), ("name", "=", "image_1920")], limit=1
+    )
+    if image_field and image_field.id not in trigger_fields.ids:
+        trigger_fields |= image_field
+
+    Company.search([]).write(
         {
-            "reprint_change_field_ids": env[
-                "res.company"
-            ].get_trigger_badge_reprint_fields(),
+            "reprint_change_field_ids": [(6, 0, trigger_fields.ids)],
         }
     )
     _logger.info("Post-migration data adjustments completed.")
