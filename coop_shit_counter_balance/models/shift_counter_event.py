f0862c272a75c8d8f8253378ebe623e9afe5216b
khoi <kkimkhoi3010@gmail.com>
2026-04-20 10:37:28 +0700

    [IMP] coop_shit_counter_balance: pre-commit auto fixes

--- coop_shit_counter_balance/models/shift_counter_event.py
+++ coop_shit_counter_balance/models/shift_counter_event.py
@@ -1,5 +1,4 @@
-
-from odoo import api, models, _
+from odoo import _, api, models
 
 
 class ShiftCounterEvent(models.Model):
@@ -7,24 +6,30 @@ class ShiftCounterEvent(models.Model):
 
     @api.model
     def cron_balance_counter(self, limit=1000):
-        args = [
-            ("display_std_points", "<", 0),
-            ("display_ftop_points", ">", 0)
-        ]
+        args = [("display_std_points", "<", 0), ("display_ftop_points", ">", 0)]
         partners = self.env["res.partner"].search(args, limit=limit)
-        SCEvent = self.env['shift.counter.event'].sudo().with_context(
-            automatic=True,
+        SCEvent = (
+            self.env["shift.counter.event"]
+            .sudo()
+            .with_context(
+                automatic=True,
+            )
         )
         for partner in partners:
             points = min(abs(partner.display_std_points), partner.display_ftop_points)
-            SCEvent.create([{
-                'name': _('Equilibrage compteur Volant >>> Fixe'),
-                'type': 'standard',
-                'partner_id': partner.id,
-                'point_qty': points,
-            }, {
-                'name': _('Equilibrage compteur Volant >>> Fixe'),
-                'type': 'ftop',
-                'partner_id': partner.id,
-                'point_qty': points*-1,
-            }])
+            SCEvent.create(
+                [
+                    {
+                        "name": _("Equilibrage compteur Volant >>> Fixe"),
+                        "type": "standard",
+                        "partner_id": partner.id,
+                        "point_qty": points,
+                    },
+                    {
+                        "name": _("Equilibrage compteur Volant >>> Fixe"),
+                        "type": "ftop",
+                        "partner_id": partner.id,
+                        "point_qty": points * -1,
+                    },
+                ]
+            )
