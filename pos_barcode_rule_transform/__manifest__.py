76ede9dd17cdf77c66797f4ff97132b3b6e2be9a
khoi <kkimkhoi3010@gmail.com>
2026-04-20 18:38:18 +0700

    [MIG] pos_barcode_rule_transform: Migration to 18.0

--- pos_barcode_rule_transform/__manifest__.py
+++ pos_barcode_rule_transform/__manifest__.py
@@ -4,14 +4,19 @@
 
 {
     "name": "Point of Sale - Barcode Rule Transform",
-    "version": "12.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Point Of Sale",
     "summary": "Transforms the value read in the barcode with a JS expression",
-    "author": "Druidoo, Odoo Community Association (OCA)",
+    "author": "Druidoo, Odoo Community Association (OCA), La Louve",
     "website": "https://github.com/AwesomeFoodCoops/odoo-production",
     "license": "AGPL-3",
     "depends": ["point_of_sale"],
-    "data": ["views/assets.xml", "views/barcode_rule.xml"],
+    "data": ["views/barcode_rule.xml"],
     "demo": ["demo/demo.xml"],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_barcode_rule_transform/static/src/js/barcode.esm.js",
+        ],
+    },
     "installable": True,
 }
