90709cc9374805f1d470e11ae0210140cf93384f
khoi <kkimkhoi3010@gmail.com>
2026-05-05 17:08:53 +0700

    [IMP] website_sale_coop_pricetag: pre-commit auto fixes

--- website_sale_coop_pricetag/__manifest__.py
+++ website_sale_coop_pricetag/__manifest__.py
@@ -1,17 +1,12 @@
 {
-    'name': 'Website Sale Show Pricetag',
-    'version': '12.0.1.0.0',
-    'category': 'Website',
-    'author': 'Trobz',
-    'website': 'https://trobz.com',
-    'license': 'AGPL-3',
-    'summary': 'Show Pricetag on the shop',
-    'depends': [
-        'website_sale',
-        'coop_default_pricetag'
-    ],
-    'data': [
-        'views/website_sale_template.xml'
-    ],
-    'installable': True,
+    "name": "Website Sale Show Pricetag",
+    "version": "12.0.1.0.0",
+    "category": "Website",
+    "author": "Trobz",
+    "website": "https://github.com/AwesomeFoodCoops/odoo-production",
+    "license": "AGPL-3",
+    "summary": "Show Pricetag on the shop",
+    "depends": ["website_sale", "coop_default_pricetag"],
+    "data": ["views/website_sale_template.xml"],
+    "installable": True,
 }
