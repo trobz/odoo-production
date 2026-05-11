881da1f497ba240c19c3ace02d6e6c85bda31446
khoi <kkimkhoi3010@gmail.com>
2026-05-11 10:52:36 +0700

    [IMP] : pre-commit auto fixes

--- website_sale_shop_access/__manifest__.py
+++ website_sale_shop_access/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    'name': 'Website Site: Show Shop for logged in user only',
-    'version': '12.0.1.0.1',
-    'category': 'Website',
-    'description': """Website Site: Show Shop for logged in user only""",
-    'author': 'Trobz',
-    'website': 'https://trobz.com',
-    'depends': [
-        'website_sale',
+    "name": "Website Site: Show Shop for logged in user only",
+    "version": "12.0.1.0.1",
+    "category": "Website",
+    "description": """Website Site: Show Shop for logged in user only""",
+    "author": "Trobz",
+    "website": "https://github.com/AwesomeFoodCoops/odoo-production",
+    "depends": [
+        "website_sale",
     ],
-    'data': [
-        'view/website_menu_view.xml',
+    "data": [
+        "view/website_menu_view.xml",
     ],
-    'test': [],
-    'installable': True,
+    "test": [],
+    "installable": True,
 }
