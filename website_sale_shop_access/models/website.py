651b9fe491bb917cf87c240eb0650dc00a7221a7
khoi <kkimkhoi3010@gmail.com>
2026-05-11 10:52:36 +0700

    [IMP] : pre-commit auto fixes

--- website_sale_shop_access/models/website.py
+++ website_sale_shop_access/models/website.py
@@ -1,4 +1,3 @@
-
 from odoo import api, fields, models
 from odoo.http import request
 
@@ -11,14 +10,15 @@ class Page(models.Model):
         comodel_name="res.groups",
         relation="res_group_website_page_rel",
         column_1="page_id",
-        column_2="group_id"
+        column_2="group_id",
     )
 
     @api.multi
     def _compute_visible(self):
         for rec in self:
             visible = rec.website_published and (
-                not rec.date_publish or rec.date_publish < fields.Datetime.now())
+                not rec.date_publish or rec.date_publish < fields.Datetime.now()
+            )
             if visible and request and rec.group_ids:
                 user_groups = set(request.env.user.groups_id)
                 if len(user_groups.intersection(rec.group_ids)) == 0:
@@ -34,15 +34,18 @@ class Menu(models.Model):
         comodel_name="res.groups",
         relation="res_group_website_menu_rel",
         column_1="menu_id",
-        column_2="group_id"
+        column_2="group_id",
     )
 
     @api.multi
     def _compute_visible(self):
         for rec in self:
             visible = True
-            if rec.page_id and not rec.page_id.sudo().is_visible and \
-                    not rec.user_has_groups('base.group_user'):
+            if (
+                rec.page_id
+                and not rec.page_id.sudo().is_visible
+                and not rec.user_has_groups("base.group_user")
+            ):
                 visible = False
             if visible and rec.group_ids:
                 user_groups = set(self.env.user.groups_id)
