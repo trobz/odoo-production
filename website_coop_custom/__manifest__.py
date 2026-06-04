b74a642bd1045fad91d291b7bdf68b053a2ecba8
phucph <phucph@trobz.com>
2026-06-04 11:03:17 +0700

    [ADD] website_coop_custom

---
+++ website_coop_custom/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Website Coop Custom",
+    "version": "18.0.1.0.0",
+    "category": "Website",
+    "summary": "Customise the website header layout for food-coop sites",
+    "author": "Trobz",
+    "website": "http://www.trobz.com",
+    "license": "LGPL-3",
+    "depends": [
+        "website",
+        "website_sale",
+    ],
+    "data": [
+        "views/website_templates.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_coop_custom/static/src/scss/website_coop_custom.scss",
+        ],
+    },
+    "installable": True,
+}
