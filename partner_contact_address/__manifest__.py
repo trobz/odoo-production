cdabc3834db5abd13eaba6a45c5acb8953e9e62c
Nguyen Minh Chien <chien@trobz.com>
2026-06-04 10:27:21 +0700

    [MIG] partner_contact_address: migration to 18.0

---
+++ partner_contact_address/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2018-Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Houssine BAKKALI - <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner contact address",
+    "version": "18.0.1.0.0",
+    "depends": ["base"],
+    "author": "Coop IT Easy SCRLfs, La Louve",
+    "category": "CRM",
+    "website": "https://github.com/AwesomeFoodCoops/odoo-production",
+    "license": "AGPL-3",
+    "summary": "Allow company contacts to use their own address",
+    "installable": True,
+}
---
+++ partner_contact_address/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module allows company contacts to keep their own address by adding
+a dedicated representative contact type on partners.
