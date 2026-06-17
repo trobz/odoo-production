260d673d75bc09b0d72aeea69f6d066fdea62678
Nguyen Minh Chien <chien@trobz.com>
2026-06-17 19:50:52 +0700

    [UPD] coop_membership: depends on spreadsheet_dashboard

--- coop_membership/__manifest__.py
+++ coop_membership/__manifest__.py
@@ -27,6 +27,7 @@
         "partner_contact_birthdate",
         "partner_contact_gender",
         "sms",
+        "spreadsheet_dashboard",
         # "web_widget_image_webcam_portrait",
     ],
     "data": [
