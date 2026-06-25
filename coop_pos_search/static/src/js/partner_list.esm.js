e895a17d0fd9303625e7ddbd83a3d80ae80cf228
phucph <phucph@trobz.com>
2026-06-25 15:14:08 +0700

    [IMP] coop_pos_search: implement search more customer

--- coop_pos_search/static/src/js/partner_list.esm.js
+++ coop_pos_search/static/src/js/partner_list.esm.js
@@ -3,19 +3,42 @@ import {patch} from "@web/core/utils/patch";
 import {unaccent} from "@web/core/utils/strings";
 
 patch(PartnerList.prototype, {
-    getPartners() {
-        const searchWord = unaccent(
+    get _queryAsNumber() {
+        const normalized = unaccent(
             (this.state.query || "").trim(),
             false
         ).toLowerCase();
-        const availablePartners = super.getPartners();
-        const numberString = searchWord.replace(/[+\s()-]/g, "");
-        const isSearchWordNumber = /^[0-9]+$/.test(numberString);
-        if (isSearchWordNumber) {
-            return availablePartners.filter((partner) =>
-                partner.exactMatch(numberString)
-            );
+        const digits = normalized.replace(/[+\s()-]/g, "");
+        return /^[0-9]+$/.test(digits) ? digits : null;
+    },
+
+    getPartners() {
+        const numberString = this._queryAsNumber;
+        if (numberString) {
+            return this.pos.models["res.partner"]
+                .getAll()
+                .filter((partner) => partner.exactMatch(numberString));
         }
-        return availablePartners;
+        return super.getPartners();
+    },
+
+    async getNewPartners() {
+        const numberString = this._queryAsNumber;
+        if (!numberString) {
+            return await super.getNewPartners();
+        }
+        const searchFields = [...this.getPhoneSearchTerms(), "barcode", "barcode_base"];
+        const domain = [
+            ...Array(searchFields.length - 1).fill("|"),
+            ...searchFields.map((field) => [
+                field,
+                "=",
+                field === "barcode_base" ? Number(numberString) : numberString,
+            ]),
+        ];
+        return await this.pos.data.searchRead("res.partner", domain, [], {
+            limit: 1,
+            offset: this.state.currentOffset,
+        });
     },
 });
