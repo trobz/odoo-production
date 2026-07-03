9473f33e9de8dede3f1cc46521d397d07126ac35
khoi <kkimkhoi3010@gmail.com>
2026-07-03 16:02:02 +0700

    [FIX] coop_point_of_sale: Hide 'Create' button in POS PartnerList based on create access right

--- coop_point_of_sale/static/src/overrides/screens/partner_list/partner_list.esm.js
+++ coop_point_of_sale/static/src/overrides/screens/partner_list/partner_list.esm.js
@@ -1,8 +1,24 @@
+import {PartnerList} from "@point_of_sale/app/screens/partner_list/partner_list";
 import {PartnerLine} from "@point_of_sale/app/screens/partner_list/partner_line/partner_line";
 import {patch} from "@web/core/utils/patch";
+import {user} from "@web/core/user";
+import {onWillStart} from "@odoo/owl";
 
 patch(PartnerLine.prototype, {
     getPartnerImageUrl(id) {
         return "/web/image?model=res.partner&id=" + id + "&field=image_128";
+    },
+});
+
+patch(PartnerList.prototype, {
+    setup() {
+        super.setup();
+        this.state.allowPartnerCreation = false;
+        onWillStart(async () => {
+            this.state.allowPartnerCreation = await user.checkAccessRight(
+                "res.partner",
+                "create"
+            );
+        });
     },
 });
