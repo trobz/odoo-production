67a80a2b57a5067f4ef8872e4c53bba7c48127ca
khoi <kkimkhoi3010@gmail.com>
2026-07-06 11:09:56 +0700

    [FIX] pos_scrap_order_origin: Restore Stock Scrap Origin config menu

--- pos_scrap_order_origin/__manifest__.py
+++ pos_scrap_order_origin/__manifest__.py
@@ -13,6 +13,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/pos_config.xml",
+        "views/stock_scrap_reason_tag_views.xml",
     ],
     "assets": {
         "point_of_sale._assets_pos": [
