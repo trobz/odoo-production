ea3d01612bca37a03751aa054c40ace7b2b8c711
Nguyen Minh Chien <chien@trobz.com>
2026-07-17 16:06:46 +0700

    [UPD]coop_point_of_sale: remove index

--- coop_point_of_sale/__manifest__.py
+++ coop_point_of_sale/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Coop - Point of Sale Custom views",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "Custom",
     "summary": """
         Customize Point of Sale Display
