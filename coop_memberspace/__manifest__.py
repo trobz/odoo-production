cd761f3b386235474cf409b9e0f3fbbb41e8173c
phucph <phucph@trobz.com>
2026-07-20 16:01:07 +0700

    [FIX] coop_memberspace: migration script for update font-size

--- coop_memberspace/__manifest__.py
+++ coop_memberspace/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Coop Memberspace",
-    "version": "18.0.1.0.2",
+    "version": "18.0.1.0.3",
     "category": "Custom",
     "author": "La Louve, Druidoo",
     "website": "https://github.com/AwesomeFoodCoops/odoo-production",
