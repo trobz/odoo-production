8128f79fb3d1d08838552cf86f7d3a8f05a1fb63
Nguyen Minh Chien <chien@trobz.com>
2026-07-23 19:08:53 +0700

    [UPD] coop_shift: pre-commit

--- coop_shift/model/stock_move_line.py
+++ coop_shift/model/stock_move_line.py
@@ -6,9 +6,12 @@ class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     def init(self):
-        super().init()
+        res = super().init()
         # Create index for better performance on stock move line queries
-        self.env.cr.execute(SQL("""
+        self.env.cr.execute(
+            SQL("""
             CREATE INDEX IF NOT EXISTS idx_sml_product_write_date
             ON stock_move_line (product_id, write_date DESC)
-        """))
+        """)
+        )
+        return res
