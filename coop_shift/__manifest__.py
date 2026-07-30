be9b90bb88da8cad558871659c4dfc8cc2830597
phucph <phucph@trobz.com>
2026-07-30 16:51:59 +0700

    [FIX] coop_shift: migrate barcode field to company dependent format

--- coop_shift/__manifest__.py
+++ coop_shift/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Coop Shift",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "category": "Tools",
     "author": ("Julien WESTE, Sylvain LE GAL, " "Cyril Gaspard, La Louve, Druidoo"),
     "website": "https://github.com/AwesomeFoodCoops/odoo-production",
