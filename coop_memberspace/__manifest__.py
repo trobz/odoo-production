0fc1d6ded97cd58c6c8b6bf8d42c3dac424ae636
phucph <phucph@trobz.com>
2026-08-04 15:46:00 +0700

    [FIX] coop_memberspace: fix carousel-indicator style

--- coop_memberspace/__manifest__.py
+++ coop_memberspace/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Coop Memberspace",
-    "version": "18.0.1.0.6",
+    "version": "18.0.1.0.8",
     "category": "Custom",
     "author": "La Louve, Druidoo",
     "website": "https://github.com/AwesomeFoodCoops/odoo-production",
