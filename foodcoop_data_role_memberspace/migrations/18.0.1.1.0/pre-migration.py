28ec44f9ebd809eeb7909fb5fd7714de000c50ef
phucph <phucph@trobz.com>
2026-08-07 14:57:24 +0700

    [IMP] foodcoop_data_role_memberspace: add migration file to set noupdate user role

---
+++ foodcoop_data_role_memberspace/migrations/18.0.1.1.0/pre-migration.py
@@ -0,0 +1,31 @@
+import logging
+
+from odoo import SUPERUSER_ID, api
+
+_logger = logging.getLogger(__name__)
+
+_DEMO_NAME_PREFIXES = ("demo_user_", "role_line_group_")
+
+
+def migrate(cr, version):
+    """Set noupdate=True on demo user and role line XML IDs.
+
+    Prevents Odoo from deleting or overwriting these records during upgrade,
+    since they are being moved to the foodcoop_data_role_demo module.
+    """
+    if not version:
+        return
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    domain = [("module", "=", "foodcoop_data_role_memberspace")]
+    domain += ["|"] * (len(_DEMO_NAME_PREFIXES) - 1)
+    for prefix in _DEMO_NAME_PREFIXES:
+        domain.append(("name", "=like", f"{prefix}%"))
+
+    records = env["ir.model.data"].search(domain)
+    if records:
+        _logger.info("Setting noupdate=True on %d demo XML ID(s)", len(records))
+        records.write({"noupdate": True})
+    else:
+        _logger.info("No demo XML IDs found to protect (already migrated)")
