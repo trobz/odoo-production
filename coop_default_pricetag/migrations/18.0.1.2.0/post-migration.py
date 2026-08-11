1f3730bfc892fb43bdbcfc6e466802c26f13ca93
phucph <phucph@trobz.com>
2026-08-11 11:55:14 +0700

    [FIX] coop_default_pricetag: fix pre-commit

--- coop_default_pricetag/migrations/18.0.1.2.0/post-migration.py
+++ coop_default_pricetag/migrations/18.0.1.2.0/post-migration.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    """Fix barcode URLs in all views, replacing /report/barcode/?type= with /report/barcode/?barcode_type=."""
+    """Fix barcode URLs in all views.
+
+    Replaces /report/barcode/?type= with /report/barcode/?barcode_type=.
+    """
     if not version:
         return
 
@@ -17,7 +19,7 @@ def migrate(cr, version):
         FROM ir_ui_view
         WHERE arch_db::text LIKE %s
         """,
-        ('%/report/barcode/?type=%',),
+        ("%/report/barcode/?type=%",),
     )
     barcode_results = cr.fetchall()
 
@@ -26,34 +28,39 @@ def migrate(cr, version):
         return
 
     barcode_updated_count = 0
-    barcode_pattern_old = '/report/barcode/?type='
-    barcode_pattern_new = '/report/barcode/?barcode_type='
+    barcode_pattern_old = "/report/barcode/?type="
+    barcode_pattern_new = "/report/barcode/?barcode_type="
 
     for view_id, arch_db in barcode_results:
         import json
-        
+
         if isinstance(arch_db, dict):
             arch_dict = arch_db
         elif isinstance(arch_db, str):
             try:
                 arch_dict = json.loads(arch_db)
-            except:
-                arch_dict = {'en_US': arch_db}
+            except json.JSONDecodeError:
+                arch_dict = {"en_US": arch_db}
         else:
-            _logger.warning("Unexpected arch_db type for view %s: %s", view_id, type(arch_db))
+            _logger.warning(
+                "Unexpected arch_db type for view %s: %s", view_id, type(arch_db)
+            )
             continue
 
         modified = False
         for lang_code, arch_content in arch_dict.items():
             if not isinstance(arch_content, str):
                 continue
-                
+
             if barcode_pattern_old in arch_content:
-                updated_content = arch_content.replace(barcode_pattern_old, barcode_pattern_new)
+                updated_content = arch_content.replace(
+                    barcode_pattern_old, barcode_pattern_new
+                )
                 arch_dict[lang_code] = updated_content
                 modified = True
                 _logger.info(
-                    "Found barcode URL pattern in view (id=%s, lang=%s), replacing with new parameter name",
+                    "Found barcode URL pattern in view (id=%s, lang=%s),"
+                    " replacing with new parameter name",
                     view_id,
                     lang_code,
                 )
