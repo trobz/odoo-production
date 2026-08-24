72ce0de129e74ab1d62c40916cf9f1fdb185296d
phucph <phucph@trobz.com>
2026-08-24 17:30:29 +0700

    [FIX] coop_membership_register_form_tracker: call as sudo to portal user can access discovery page

--- coop_membership_register_form_tracker/controllers/main.py
+++ coop_membership_register_form_tracker/controllers/main.py
@@ -15,7 +15,7 @@ class WebsiteRegisterMeetingTracker(WebsiteRegisterMeeting):
     def _prepare_register_form_vals(self, vals):
         vals = super()._prepare_register_form_vals(vals)
         # Add utm.source
-        source_datas = request.env["utm.source"]._get_member_register_sources()
+        source_datas = request.env["utm.source"].sudo()._get_member_register_sources()
         vals["source_datas"] = source_datas
         vals["company_name"] = request.env.user.company_id.name
         return vals
@@ -33,6 +33,8 @@ class WebsiteRegisterMeetingTracker(WebsiteRegisterMeeting):
             sids.append(int(post.get(k)))
         # Check other source
         if post.get("other_source"):
-            source = request.env["utm.source"].create({"name": post["other_source"]})
+            source = (
+                request.env["utm.source"].sudo().create({"name": post["other_source"]})
+            )
             sids.append(source.id)
         return sids
