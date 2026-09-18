dddaddfbc7b818ded3a593ca5b7164c575478d12
khoi <kkimkhoi3010@gmail.com>
2026-09-18 12:42:28 +0700

    [MIG] website_category_publish: add post-migration script

--- website_category_publish/__manifest__.py
+++ website_category_publish/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Website Category Publish",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "author": "Trobz, La Louve",
     "license": "AGPL-3",
     "category": "Website",
